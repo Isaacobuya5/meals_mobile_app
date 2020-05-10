f6e50cb5e0ef95233a0ac2bc859102a05441c065
Isaac Obuya <Szack4477@gmail.com>
2020-05-10 18:08:57 +0300

    ereplace links in main drawer with pushReplacementNamed to replace pages

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import './main_drawer.dart';
 
 class Filters extends StatelessWidget {
 
   static const routeName = '/filters';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Filters')
       ),
+      drawer: MainDrawer(),
         body: Center(
         child: Text('Filters Screen')
       ),
